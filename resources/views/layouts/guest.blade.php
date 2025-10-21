d3e6469c947ed4093806d9238adb3fd709040454
Sherlin <smk.dev20@gmail.com>
2025-10-21 15:59:44 -0700

    add metrics

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,5 +20,22 @@
                 {{ $slot }}
             </div>
         </div>
+        <!-- Default Statcounter code for KitchenTales
+            https://kitchentales.webserve.xyz -->
+            <script type="text/javascript">
+            var sc_project=13177571; 
+            var sc_invisible=1; 
+            var sc_security="18da79b1"; 
+            </script>
+            <script type="text/javascript"
+            src="https://www.statcounter.com/counter/counter.js"
+            async></script>
+            <noscript><div class="statcounter"><a title="Web Analytics"
+            href="https://statcounter.com/" target="_blank"><img
+            class="statcounter"
+            src="https://c.statcounter.com/13177571/0/18da79b1/1/"
+            alt="Web Analytics"
+            referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+            <!-- End of Statcounter Code -->
     </body>
 </html>
